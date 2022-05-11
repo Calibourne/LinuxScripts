19cdeecb1f3a000956a5758d8e9a9c83aab09a14
Eddie Kanevsky <eddiekanevsky@gmail.com>
2022-05-11 10:26:12 +0300

    started the disk partition

--- script.sh
+++ script.sh
@@ -2,13 +2,27 @@
 
 clear
 
+# Welcome Screen
+
 echo -ne "
-------------------------------------------------------------------------------------------
-     █████   ████   ██       ██ ██████   ██████  ██    ██ ███████  ██      ██ ████████
-    ██      ██  ██  ██       ██ ██   ██ ██    ██ ██    ██ ██    ██ ████    ██ ██      
-    ██     ██    ██ ██       ██ ██████  ██    ██ ██    ██ ██    ██ ██  ██  ██ ██████  
-    ██     ████████ ██       ██ ██   ██ ██    ██ ██    ██ ██████   ██    ████ ██      
-     █████ ██    ██ ████████ ██ ██████   ██████   ██████  ██    ██ ██      ██ ████████
-----------------------Convinient Script To Install Minimal Arch---------------------------
+╔════════════════════════════════════════════════════════════════════════════════════════════════════╗
+║     ██████    ████    ██        ██  ██████    ██████   ██    ██  ███████   ██      ██  ████████    ║
+║    ██        ██  ██   ██        ██  ██   ██  ██    ██  ██    ██  ██    ██  ████    ██  ██          ║
+║    ██       ██    ██  ██        ██  ██████   ██    ██  ██    ██  ██    ██  ██  ██  ██  ██████      ║
+║    ██       ████████  ██        ██  ██   ██  ██    ██  ██    ██  ██████    ██    ████  ██          ║
+║     ██████  ██    ██  ████████  ██  ██████    ██████    ██████   ██    ██  ██      ██  ████████    ║
+╚═════════════════════════ A Convinient Script To Install Minimal Arch ══════════════════════════════╝
 "
 
+echo "Welcome to my Arch minimal-install script!"
+echo "We invite you to make yourself at comfort: prepare some drink, some goodies, and let's get started" 
+echo "Press any key when you're ready..."
+
+read -n 1 -s && clear
+
+# Disk Partition
+echo "Let's start with disk partition: "
+sleep 3
+echo "From the following disks, select the disk you want to partition:"
+lsblk
+# cfdisk
